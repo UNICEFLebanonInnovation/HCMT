3f6ae393e688d02388eb0144b81a42a21447fb9f
James Cranwell-Ward <jcranwellward@gmail.com>
2017-11-03 15:39:35 +0200

    Repo cleanup

---
+++ tasks.py
@@ -0,0 +1,57 @@
+import json
+import bson
+import click
+from os import path
+import requests
+from requests.auth import HTTPBasicAuth
+
+from app import app, db
+
+
+def _get_aggreagation(name):
+    with open(name + '.json') as json_data:
+        return json.load(json_data)
+
+
+@app.cli.command()
+def import_form_data(
+        url='https://kc.humanitarianresponse.info/api/v1/data',
+        kobo_id='',
+        username='',
+        password='',
+        collection='submissions'
+):
+    """Initialize the database."""
+    click.echo('Staring import from Kobo...')
+
+    data = requests.get(
+        path.join(url, kobo_id),
+        auth=HTTPBasicAuth(username, password))
+
+    db.connection.get_default_database()[collection].drop()
+    db.connection.get_default_database()[collection].insert_many(data.json())
+
+
+@app.cli.command()
+def convert_to_integer(
+        collection='submissions',
+        field_name='Metadata_section/site_data/sample_tents'
+):
+    for form in  db.connection.get_default_database()[collection].find():
+        if field_name not in form:
+            continue
+        form[field_name] = int(form[field_name])
+        db.connection.get_default_database()[collection].save(form)
+
+
+@app.cli.command()
+def run_aggregation(
+        collection='submissions',
+        file_name='group_by_site'
+):
+    click.echo(db.connection.get_default_database().command(
+        'aggregate',
+        collection,
+        pipeline=_get_aggreagation(file_name),
+        allowDiskUse=True
+    ))
